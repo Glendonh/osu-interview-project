e800b60ad44d3e4c1e3e8a2bfaf3683efcf496b7
Glendon Hardin <glendon.hardin@alseageo.net>
2020-06-06 12:26:58 -0700

    route / to index file

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port = 8080
 const data = require("./data.json")
 
 app.use(express.static(__dirname + "/public"))
+app.get("/", (req, res) => {
+  app.sendFile("./public/index.html")
+})
 
 app.get("/cities", (req, res) => res.send(data))
 
